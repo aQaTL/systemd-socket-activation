2c53a0f340e7bc5f35d178f21de754ceefae054d
aQaTL <mmsoltys@outlook.com>
2021-08-01 16:03:19 +0200

    Allow linking systemd instead of using dlopen

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,86 @@
-use crate::Error::LibLoading;
 use std::net::TcpListener;
 use std::os::unix::prelude::FromRawFd;
 
 const SD_LISTEN_FDS_START: i32 = 3;
 
-type SdListenFdsFunc = unsafe extern "C" fn(unset_environment: i32) -> i32;
-
-const SD_LISTEN_FDS_FUNC_NAME: &[u8] = b"sd_listen_fds\0";
-
 #[derive(Debug)]
 pub enum Error {
-	LibLoading(libloading::Error),
-	LibLoadingFailedToLoadSystemd(String),
-	Systemd(std::io::Error),
+    #[cfg(feature = "dlopen")]
+    LibLoading(libloading::Error),
+    #[cfg(feature = "dlopen")]
+    LibLoadingFailedToLoadSystemd(String),
+    Systemd(std::io::Error),
 }
 
+#[cfg(feature = "dlopen")]
 impl From<libloading::Error> for Error {
-	fn from(e: libloading::Error) -> Self {
-		Error::LibLoading(e)
-	}
+    fn from(e: libloading::Error) -> Self {
+        Error::LibLoading(e)
+    }
 }
 
+#[cfg(not(any(feature = "dlopen", feature = "dynlink")))]
 pub fn systemd_socket_activation() -> Result<Vec<TcpListener>, Error> {
-	let nfds = unsafe {
-		let systemd_lib = libloading::Library::new("libsystemd.so.0").map_err(|err| match err {
-			dlopen_err @ libloading::Error::DlOpen { .. } => {
-				Error::LibLoadingFailedToLoadSystemd(dlopen_err.to_string())
-			}
-			e => LibLoading(e),
-		})?;
+    unimplemented!("Enable either \"dlopen\" or \"dynlink\" feature to use this crate")
+}
 
-		let sd_listen_fds: libloading::Symbol<SdListenFdsFunc> =
-			systemd_lib.get(SD_LISTEN_FDS_FUNC_NAME)?;
+#[cfg(feature = "dlopen")]
+pub fn systemd_socket_activation() -> Result<Vec<TcpListener>, Error> {
+    type SdListenFdsFunc = unsafe extern "C" fn(unset_environment: i32) -> i32;
 
-		sd_listen_fds(false as i32)
-	};
+    const SD_LISTEN_FDS_FUNC_NAME: &[u8] = b"sd_listen_fds\0";
 
-	if nfds < 0 {
-		return Err(Error::Systemd(std::io::Error::from_raw_os_error(nfds)));
-	}
+    let nfds = unsafe {
+        let systemd_lib = libloading::Library::new("libsystemd.so.0").map_err(|err| match err {
+            dlopen_err @ libloading::Error::DlOpen { .. } => {
+                Error::LibLoadingFailedToLoadSystemd(dlopen_err.to_string())
+            }
+            e => Error::LibLoading(e),
+        })?;
 
-	let listeners: Vec<TcpListener> = (SD_LISTEN_FDS_START..(SD_LISTEN_FDS_START + nfds))
-		.map(|fd| unsafe { TcpListener::from_raw_fd(fd) })
-		.collect();
+        let sd_listen_fds: libloading::Symbol<SdListenFdsFunc> =
+            systemd_lib.get(SD_LISTEN_FDS_FUNC_NAME)?;
 
-	Ok(listeners)
+        sd_listen_fds(false as i32)
+    };
+
+    if nfds < 0 {
+        return Err(Error::Systemd(std::io::Error::from_raw_os_error(nfds)));
+    }
+
+    let listeners: Vec<TcpListener> = (SD_LISTEN_FDS_START..(SD_LISTEN_FDS_START + nfds))
+        .map(|fd| unsafe { TcpListener::from_raw_fd(fd) })
+        .collect();
+
+    Ok(listeners)
+}
+
+#[cfg(feature = "dynlink")]
+pub fn systemd_socket_activation() -> Result<Vec<TcpListener>, Error> {
+    #[link(name = "systemd")]
+    extern "C" {
+        fn sd_listen_fds(unset_environment: i32) -> i32;
+    }
+
+    let nfds = unsafe { sd_listen_fds(false as i32) };
+
+    if nfds < 0 {
+        return Err(Error::Systemd(std::io::Error::from_raw_os_error(nfds)));
+    }
+
+    let listeners: Vec<TcpListener> = (SD_LISTEN_FDS_START..(SD_LISTEN_FDS_START + nfds))
+        .map(|fd| unsafe { TcpListener::from_raw_fd(fd) })
+        .collect();
+
+    Ok(listeners)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn does_it_work() {
+        assert!(systemd_socket_activation().is_ok());
+    }
 }
